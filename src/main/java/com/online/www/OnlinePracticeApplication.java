bdeccd352a60ea4b96870eae414e3d47add058f1
GoodBoyCoder <13682768293@163.com>
2021-11-16 14:09:13 +0800

    test: 测试镜像构建v3

---
+++ src/main/java/com/online/www/OnlinePracticeApplication.java
@@ -0,0 +1,15 @@
+package com.online.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author GoodBoyCoder
+ * @date 2021-11-16
+ */
+@SpringBootApplication
+public class OnlinePracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinePracticeApplication.class, args);
+    }
+}
