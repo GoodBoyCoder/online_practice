0e90781205711520ee98ddac65f47a790200f31f
GoodBoy250 <41784017+GoodBoyCoder@users.noreply.github.com>
2021-11-19 21:59:49 +0800

    feature:补充user_question实体类 (#4)

---
+++ src/main/java/com/online/www/service/UserQuestionService.java
@@ -0,0 +1,11 @@
+package com.online.www.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.online.www.pojo.po.UserQuestion;
+
+/**
+ * @author GoodBoy
+ * @date 2021-11-19
+ */
+public interface UserQuestionService extends IService<UserQuestion> {
+}
